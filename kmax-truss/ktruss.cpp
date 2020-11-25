e35640b9a7d171afdd2116bb77b589b3faba05ad
ChenJG <dlvguo@qq.com>
2020-11-25 21:21:51 +0800

    uplasted

---
+++ kmax-truss/ktruss.cpp
@@ -0,0 +1,14 @@
+#define WIN32
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <omp.h> 
+#include<time.h>
+// #include <sys/time.h>
+#include <functional>
+#include <iostream>
+#include <unordered_map>
+#include <climits>
+#include<vector>
+
+using namespace std;
